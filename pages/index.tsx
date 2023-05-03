8dc05e0781e68e19edbed9f8c2903bf0f7bf6f08
Dom Gaulton <domgaulton@gmail.com>
2023-05-03 21:12:31 +0100

    dark mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 export default function Home() {
   return (
-    <main
-      className={`flex h-screen flex-col items-center justify-center p-24`}
-    >
+    <main className={`flex h-screen flex-col items-center justify-center p-24 dark:bg-black dark:text-white`}>
       <h1 className="text-4xl mb-4">🚀 domgaulton.tech</h1>
       <p>Coming Soon...</p>
     </main>
