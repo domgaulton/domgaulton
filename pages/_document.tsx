f15404bf2b76965d7da0978fa978a30ec1f6f218
Dom Gaulton <domgaulton@gmail.com>
2023-05-03 21:43:23 +0100

    feat: seo and github

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>domgaulton.tech</title>
+        <meta name="description" content={'London based senior full stack developer'} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,10 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>DomGaulton.Tech</title>
-        <meta name="description" content={'London based senior full stack developer'} />
-      </Head>
-      <main className={`flex h-screen flex-col items-center justify-center p-24 dark:bg-black dark:text-white`}>
-      <h1 className="text-4xl mb-4">🚀 DomGaulton.Tech</h1>
+    <main className={`flex h-screen flex-col items-center justify-center p-24 dark:bg-black dark:text-white`}>
+      <h1 className="text-4xl mb-4">🚀 domgaulton.tech</h1>
       <p className="text-xl mb-2">Coming Soon...</p>
       <a href="https://github.com/domgaulton" target="_blank"><p>github.com/domgaulton</p></a>
     </main>
-    </>
   )
 }
