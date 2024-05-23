6d3f426daf0eca46f47fa3a81c65a4bcc432501d
Dom Gaulton <domgaulton@gmail.com>
2024-05-23 15:42:25 +0100

    revert moxies pubs to subdomain (#5)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  async redirects() {
+    return [
+      {
+        source: '/moxies-pubs',
+        destination: 'https://moxies-pubs.domgaulton.tech',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
