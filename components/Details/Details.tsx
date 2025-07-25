9fae89023574cab7078dd0f57c728557ee678bc0
Dom Gaulton <domgaulton@gmail.com>
2025-07-25 11:18:36 +0100

    feat: updated cv

--- components/Details/Details.tsx
+++ components/Details/Details.tsx
@@ -2,13 +2,13 @@ const Details = () => {
   return (
     <>
       <h2 className="text-xl mb-4">
-        Full Stack Software & Product Engineer (experience in both staff and lead roles) 
+        Full Stack Software & Product-Centric Engineer (experience in both IC and Management positions) 
       </h2>
       <p className="text-lg mb-4">
         Strong track record at both agencies and start-ups, experienced in building MVPs and delivering bespoke full stack applications. 
       </p>
       <p className="text-lg mb-4">
-        Played a key role at Lottie&apos;s early-stage start-up, contributing to its growth from inception through to a successful Series A funding round.
+        Played a key role at health-tech start-up Lottie; contributing to its early-stage growth through to a successful Series A funding round and scale-up company.
       </p>
     </>
   )
